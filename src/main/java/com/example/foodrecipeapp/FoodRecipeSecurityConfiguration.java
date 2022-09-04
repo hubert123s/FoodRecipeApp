0cd4c603154ec0cd7151f95ee1aa1bf70a732e6b
hubert123 <hubert.antoniszyn98@gmail.com>
2022-09-04 21:47:55 +0200

    enable Spring Security

---
+++ src/main/java/com/example/foodrecipeapp/FoodRecipeSecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.foodrecipeapp;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class FoodRecipeSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("hubert@exampl.org")
+                .password("{noop}123")
+                .roles("USER")
+                .and()
+                .withUser("ADMIN")
+                .password("{noop}123")
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .mvcMatchers(HttpMethod.GET,"/meal/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
